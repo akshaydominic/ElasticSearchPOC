38bddb88ec2068937591952c57e4fa1278f75350
Akshay <adominic21@gmail.com>
2022-03-18 01:52:59 +0530

    config elastic search added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector';
+import { SearchProvider, Results, SearchBox } from '@elastic/react-search-ui';
+import { Layout } from '@elastic/react-search-ui-views';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import '@elastic/react-search-ui-views/lib/styles/styles.css';
+
+const connector = new AppSearchAPIConnector({
+	searchKey: 'search-229rksjgu5nb5gm75fq9foxp',
+	engineName: 'student-elasticsearch',
+	endpointBase: 'https://student-elasticsearch.ent.us-central1.gcp.cloud.es.io',
+	cacheResponses: false
+});
+export default function App() {
+	return (
+		<SearchProvider
+			config={{
+				apiConnector: connector
+			}}
+		>
+			<div className="App">
+				<Layout header={<SearchBox />} bodyContent={<Results titleField="firstname" />} />
+			</div>
+		</SearchProvider>
+	);
 }
-
-export default App;
